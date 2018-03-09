42df616a72f6713b158b6bb40aab6d0d343d10e9
Jessica Ju <jju@csod.com>
2018-03-09 15:45:18 -0800

    first commit

---
+++ src/pearls/QuestionOne.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pearls;
+
+/**
+ * @author jju
+ *
+ */
+public class QuestionOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); 
+		// TODO Auto-generated method stub
+
+	}
+
+}
